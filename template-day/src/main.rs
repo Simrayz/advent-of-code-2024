98ea090e7da3bbc3a4f4b6c9143fefed61a59545
Simen Abelsen <simen.nordbo.abelsen@brilliant.no>
2024-12-11 12:02:59 +0100

    🧑‍💻 Add template for new days

---
+++ template-day/src/main.rs
@@ -0,0 +1,13 @@
+use template_day::*;
+
+fn main() -> miette::Result<()> {
+    let input: &str = include_str!("../input2.txt");
+
+    let solution = part1::process(input)?;
+    println!("Part 1: {}", solution);
+
+    let solution = part2::process(input)?;
+    println!("Part 2: {}", solution);
+
+    Ok(())
+}
